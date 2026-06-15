269e0e8b2db05cfcfa3d71ff5d1b8e826c8d6757
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-15 18:56:55 +0530

    feat: add data types in python

---
+++ python/fundamentals/data_types.py
@@ -0,0 +1,20 @@
+# data types in python 
+
+name = "Tejas"                     # str
+age = 25                           # int
+height = 5.9                       # float
+is_student = True                  # bool
+skills = ["Python", "C", "C++"]    # list
+colors = ("Red", "Green")          # tuple
+numbers = {1, 2, 3, 4}             # set
+student = {"name": "Tejas", "age": 25}  # dict
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(skills))
+print(type(colors))
+print(type(numbers))
+print(type(student))
+
