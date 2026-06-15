7eabd077175bf51315526c134ae2c922a8a0584a
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-15 18:46:46 +0530

    feat: add variables in python

---
+++ python/fundamentals/variables.py
@@ -0,0 +1,13 @@
+# variables in python
+
+first_name = "Tejas"
+last_name = "Kalmegh"
+age = 25
+city = "Pune"
+is_student = True
+
+print("First name: ", first_name)
+print("Last name: ", last_name)
+print("Age: ", age)
+print("City: ", city)
+print("Student: ", is_student)
